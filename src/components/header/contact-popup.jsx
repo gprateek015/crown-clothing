df9126bb4d24015f0e6515cb04b7a6e0c6fe66cd
Prateek Goyal <prateek@Prateeks-MacBook-Pro.local>
2025-07-07 23:52:50 +0530

    updated contact popup

--- src/components/header/contact-popup.jsx
+++ src/components/header/contact-popup.jsx
@@ -10,17 +10,17 @@ const ContactPopup = ({ open }) => {
         display: open ? 'block' : 'none'
       }}
     >
-      <div className='name'>Chesta Hukwani</div>
+      <div className='name'>Prateek Goyal</div>
       <div className='row'>
         <div className='label'>Email:</div>
-        <a href='mailto:chestahukwani@gmail.com' className='value'>
-          chestahukwani@gmail.com
+        <a href='mailto:gprateek316@gmail.com' className='value'>
+          gprateek3@gmail.com
         </a>
       </div>
       <div className='row'>
         <div className='label'>Phone:</div>
-        <a href='tel:6266340208' className='value'>
-          6266340208
+        <a href='tel:7999956584' className='value'>
+        7999956584
         </a>
       </div>
     </div>
